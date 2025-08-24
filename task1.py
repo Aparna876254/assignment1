62d3ec488351a0b90c63f0d590afacfb351a7763
Aparna876254 <aptudavekar@gmail.com>
2025-08-24 21:26:08 +0530

    Added Task 1 and Task 2 scripts

---
+++ task1.py
@@ -0,0 +1,6 @@
+a=int(input('enter the first number :'))
+b=int(input('enter the second number :'))
+print("\n\naddition :",a+b)
+print("subtraction :",a-b)
+print("Multiplication :",a*b)
+print("division :",a/b
---
+++ task2.py
@@ -0,0 +1,4 @@
+a=input('enter the first name :')
+b=input('enter the last name :')
+
+print('hello ' + (a + ' ' + b) +'! Welcome to python program' )
